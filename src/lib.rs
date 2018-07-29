6e072bb5b3fb059ddf4e6e4e3f1c21f93bbfd95e
lizichen <lizichen@lizichendeMacBook-Pro.local>
2018-07-29 16:28:53 +0800

    [ADD: jni func]

--- src/lib.rs
+++ src/lib.rs
@@ -86,6 +86,29 @@ static SIMPLE_FRAGMENT: &'static str = "
     }
 ";
 
+/// Expose the JNI interface for android below
+#[cfg(target_os="android")]
+#[allow(non_snake_case)]
+pub mod android {
+    extern crate jni;
+
+    use super::*;
+    use self::jni::JNIEnv;
+    use self::jni::objects::{JClass, JString};
+    use self::jni::sys::{jstring};
+
+    #[no_mangle]
+    pub unsafe extern fn Java_com_example_vampire_opengl_Rust_draw(env: JNIEnv, _: JClass) {
+        // Our Java companion code might pass-in "world" as a string, hence the name.
+//        let world = rust_greeting(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
+//        // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
+//        let world_ptr = CString::from_raw(world);
+//        let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
+
+        init_env_rs();
+    }
+}
+
 #[no_mangle]
 pub extern fn init_env_rs() {
 //    let mut screen_width = 1024_usize;
